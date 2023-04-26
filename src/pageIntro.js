2df44f1a201333880e28c791bf2a5dc83323dca3
sahil s <sahilsrivastava@hotmail.com>
2023-04-26 11:04:51 -0400

    006

--- src/pageIntro.js
+++ src/pageIntro.js
@@ -6,6 +6,8 @@ img src "/home/sahil/restPage/src/pizza.png"
 /*Trying to see how to import, which files to set
 in the HTML doc as type=module to be able to use import/export.
 */
+import pizza from './pizza.png';
+//now lets see if importing of the image worked.
 function pageLoad(){
 let header1 = document.createElement('h1');    
 header1.textContent='Greatest Napolitan pizza, this side of the naples!';
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const { type } = require('os');
 const path = require('path');
 
 module.exports = {
@@ -5,5 +6,11 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  //this lets you load image files in your 
+  //js code.
+  module:{
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset/resource',
   },
 };
