fcaea5d38d5db150dba4a44460b1df6eeee6f550
sahil s <sahilsrivastava@hotmail.com>
2023-04-25 20:05:22 -0400

    005

--- src/pageIntro.js
+++ src/pageIntro.js
@@ -2,3 +2,14 @@
 img src "/home/sahil/restPage/src/pizza.png"
 */
 
+//need to import the pizza image here.
+/*Trying to see how to import, which files to set
+in the HTML doc as type=module to be able to use import/export.
+*/
+function pageLoad(){
+let header1 = document.createElement('h1');    
+header1.textContent='Greatest Napolitan pizza, this side of the naples!';
+
+let image1 = document.createElement('img');
+
+}
