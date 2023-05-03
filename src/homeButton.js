e9b0aa41f0aec5c2c4e590d4528d0340ab970c3c
sahil s <sahilsrivastava@hotmail.com>
2023-05-02 20:11:46 -0400

    017

--- src/homeButton.js
+++ src/homeButton.js
@@ -4,4 +4,13 @@ various pages without leaving the actual page?
 tab 1)Intro, hours, location
 tab 2)Menu
 tab 3)Contact
- */
+ 
+We're here.
+*/
+
+function createButtons(){
+    let homeBtn = document.createElement('button');
+   
+
+
+}
