28ae3910f7c176504d9e2772b032534dfcb17d2c
sahil s <sahilsrivastava@hotmail.com>
2023-05-03 11:26:08 -0400

    018

--- src/homeButton.js
+++ src/homeButton.js
@@ -1,14 +1,18 @@
 /*Creating buttons that allow you to 'tab' into
 various pages without leaving the actual page?
 
-tab 1)Intro, hours, location
+tab 1)Home (with Intro, hours, location)
 tab 2)Menu
 tab 3)Contact
  
+Create a seperate module for each 'tab' and export and import
+them all into index.js
+
 We're here.
 */
 
-function createButtons(){
+function createHomeButton(){
+    let homeDiv= document.createElement('div');
     let homeBtn = document.createElement('button');
    
 
