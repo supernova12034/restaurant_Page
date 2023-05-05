a45ea9c6f6ab6759de6a5d7745cc89568d4cde6a
sahil s <sahilsrivastava@hotmail.com>
2023-05-05 11:03:25 -0400

    025

--- src/homeButton.js
+++ src/homeButton.js
@@ -21,12 +21,17 @@ export function createHomeButton(){
     homeBtn.textContent = 'Home';
 
     intro.textContent = "At Bobby's pizza we use authentic recipes " +
-    ' and fresh ingredients to create the meltiest, tastiest, handcrafted ' +
-    ' pizza possible for you!';
+    'and fresh ingredients to create the meltiest, tastiest, handcrafted ' +
+    'pizza possible for you!';
 
     hours.textContent = 'Hours: \n' +
     'Monday: Closed \n' +
-    'Tuesday: 12pm-10pm \n';
+    'Tuesday: 12pm-9pm \n' +
+    'Wednesday: 12pm-9pm \n' +
+    'Thursday 12pm-9pm \n' + 
+    'Friday 12pm-11pm \n' +
+    'Saturday 12pm-11pm \n' +
+    'Sunday 12pm-11pm' ;
 
     //We're here, creating content for our tabs,
     
