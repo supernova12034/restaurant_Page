b3618ed5b4149641715637ed4ccc27fa8c601b4e
sahil s <sahilsrivastava@hotmail.com>
2023-05-11 11:22:50 -0400

    026

--- src/homeButton.js
+++ src/homeButton.js
@@ -14,8 +14,21 @@ We're here. Just creating the actual content for the 'tabs'.
 export function createHomeButton(){
     let homeDiv= document.createElement('div');
     let intro = document.createElement('div');
+
     let hours = document.createElement('div');
+    let day1 = document.createElement('div');
+    let day2 = document.createElement('div');
+    let day3 = document.createElement('div');
+    let day4 = document.createElement('div');
+    let day5 = document.createElement('div');
+    let day6 = document.createElement('div');
+    let day7 = document.createElement('div');
+    let lineBreak = document.createElement('br');
+    
     let location = document.createElement('div');
+    let location2 = document.createElement('div');
+    let location3 = document.createElement('div');
+    let location4 = document.createElement('div');
 
     let homeBtn = document.createElement('button');
     homeBtn.textContent = 'Home';
@@ -24,19 +37,41 @@ export function createHomeButton(){
     'and fresh ingredients to create the meltiest, tastiest, handcrafted ' +
     'pizza possible for you!';
 
-    hours.textContent = 'Hours: \n' +
-    'Monday: Closed \n' +
-    'Tuesday: 12pm-9pm \n' +
-    'Wednesday: 12pm-9pm \n' +
-    'Thursday 12pm-9pm \n' + 
-    'Friday 12pm-11pm \n' +
-    'Saturday 12pm-11pm \n' +
-    'Sunday 12pm-11pm' ;
+    
+    day1.textContent = 'Monday: Closed ';
+    day2.textContent = 'Tuesday: 12pm-9pm';
+    day3.textContent = 'Wednesday: 12pm-9pm';
+    day4.textContent = 'Thursday 12pm-9pm';
+    day5.textContent = 'Friday 12pm-11pm';
+    day6.textContent = 'Saturday 12pm-11pm';
+    day7.textContent = 'Sunday 12pm-11pm';
 
+    hours.append(lineBreak,lineBreak,day1,lineBreak,day2,lineBreak,day3,lineBreak,day4,lineBreak,day5,lineBreak,day6,lineBreak,day7,lineBreak);
+
+    location.append(lineBreak);
+    location.textContent = 'We are conviniently located ';
+    location.append(lineBreak);
+    location2.textContent = 'at platform 9 3/4,';
+    location.append(location2);
+    location.append(lineBreak);
+    location3.textContent = 'at 1 Kings Cross Street, ';
+    location.append(location3);
+    location.append(lineBreak);
+    location4.textContent = 'London, UK.';
+    location.append(location4);
     //We're here, creating content for our tabs,
+    //tweak the styling etc.
+
+    //-Apparently textContent cant be split up into
+    //multiple lines...might have to try different
+    //approach
+    //-Might have to create elements for each line/day and
+    //append them into the hours div? (It worked!)
+   
+    
     
 
-    homeDiv.append(homeBtn);     
+    homeDiv.append(homeBtn,lineBreak,intro,hours,location);     
     document.body.append(homeDiv);//it worked!
     console.log('homeButton.js');
 }
