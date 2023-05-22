230dab5083826119a3acff6a7bda6e499660e787
sahil s <sahilsrivastava@hotmail.com>
2023-05-22 09:09:09 -0400

    046

--- src/menuButton.js
+++ src/menuButton.js
@@ -1,3 +1,5 @@
+import {createHomeButton as homeB } from './homeButton';
+
 /*Creating buttons that allow you to 'tab' into
 various pages without leaving the actual page?
 
@@ -14,6 +16,13 @@ them all into index.js
 
 export function createMenuButton(){
 //We're here, have to create menu button, and see that it works right.
+//where will the menu button be appended to? a new div? the other 
+//div in the different module?? Will we have to import the other module?
+//Already imported other module.
+let menuMainDiv = document.createElement('div');
+
+let menuBtn = document.createElement('button');
+
 
 
 }
